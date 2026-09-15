d43437ee9ee11ef86878ccbedf6b4586f32e9248
JohnPickles59 <grayday11@icloud.com>
2026-09-15 19:01:11 +0000

    update

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,7 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("°C = (23.6°F - 32) x 5/9"); 
+		System.out.println((23.6 - 32.0) * 5.0/9.0 + "°F");
 	}
 }
--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -7,8 +7,24 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter age: ");
+		String age = sc.nextLine();
+		System.out.println("Enter birthday month: ");
+		int bdaymonth = sc.nextInt();
+		System.out.println("Enter birthday day: ");
+		int bdayday = sc.nextInt();
+		System.out.println("Enter birthday year: ");
+		int bdayyear = sc.nextInt();
+		System.out.println("How much is a buck fifty: ");
+		double buckfifty = sc.nextDouble();
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Birthday: " + bdaymonth + " " + bdayday + ", " + bdayyear);
+		System.out.println("A buck fifty is: $" + buckfifty);
 	}
 }
